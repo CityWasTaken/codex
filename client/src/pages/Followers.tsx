f5973b4c3954337686d1f133f598c670926e66b3
Funkyskep <phyrowolf24@gmail.com>
2024-12-02 12:36:51 -0500

    save

---
+++ client/src/pages/Followers.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Row, Col, Container, Card,ListGroup } from "react-bootstrap";
+
+
+const Followers = () => {
+
+
+
+
+
+
+    return (
+        <Container>
+            <Card>
+                <Card.Body></Card.Body>
+            </Card>
+        </Container>
+}
+
