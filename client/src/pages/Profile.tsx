829bff1ebaf4ee41e5d4f8e604d84a163ec10159
CityWasTaken <smithfaheed@gmail.com>
2024-12-03 12:38:50 -0500

    added uniqueness to user profiles

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,26 +1,34 @@
 import { Row, Col, Container, Card, Button } from "react-bootstrap";
 import { useQuery } from "@apollo/client";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
-import { GET_USER } from "../graphql/queries";
+import { GET_USER_INFO } from "../graphql/queries";
 import { useStore } from "../store/index";
 import { Post } from "../interfaces";
-
-
 
 /* making the functionality of the page before pretting it up(writing so i dont confuse myself) */
  
 
 function Profile() {
+    const { username } = useParams<{ username: string }>();
+
+    //error handling and loading screens
+    if (!username) {
+        return <div>Loading...</div>;
+    }
+
     //get the user from the store
     const { state } = useStore()!;
-    const { data, loading, error } = useQuery(GET_USER, {
-        variables: { id: state.user._id },
+    const { data } = useQuery(GET_USER_INFO, {
+        variables: {
+          username: username  
+        }
     });
 
     //more error handling and loading screens
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error</p>;
+    if (!data) {
+        return <div>Loading...</div>;
+    }
 
     const user = data.user;
     
@@ -29,12 +37,10 @@ function Profile() {
 
     <Container fluid={true} className="mt-a">
         <Row>
-            //display user info
+            {/* display user info */}
             <Col md="4">
                 <Card.Body>
-                    <Card.Title>{user.username}</Card.Title>
-                    //do we need an email? ill put it in for now
-                    <Card.Text>{user.email}</Card.Text>
+                    <Card.Title>{data.getUserInfo.user.username}</Card.Title>
 
                     <Link to="/followers">
                         <Button variant="primary" className="me-2">Followers</Button>
@@ -47,15 +53,15 @@ function Profile() {
                 </Card.Body>
             </Col>
 
-            //display posts
+            {/* display posts */}
             <Col md="8">
                 <h2 className="fw-light">My Posts</h2>
                 <hr />
                 <Row className = "my-4">
-                    {user.posts.length === 0 ?(
+                    {data.getUserInfo.user.posts.length === 0 ?(
                         <p>No posts yet.</p>
                     ):(
-                        user.posts.map((post: Post) => (
+                        data.getUserInfo.user.posts.map((post: Post) => (
                             <Col lg="6" md="12" key={post._id} className="mb-4">
                                 <Card className="h-100">
                                     <Card.Body>
@@ -74,3 +80,11 @@ function Profile() {
 }
 
 export default Profile;
+
+/*
+    <div>
+        <h1>{data.getUserInfo.user.username}'s Profile</h1>
+        <p>Followers: {data.getUserInfo.user.followers.map((user: any) => user.username).join(', ')}</p>
+        <p>Post count: {data.getUserInfo.user.posts.length}</p>
+    </div>
+*/
