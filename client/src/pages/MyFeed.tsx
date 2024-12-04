a3565846282d367dfc4cd192397ea96a10cd086f
Funkyskep <phyrowolf24@gmail.com>
2024-12-04 14:25:55 -0500

    made my feed

--- client/src/pages/MyFeed.tsx
+++ client/src/pages/MyFeed.tsx
@@ -1,1 +1,36 @@
-import
+import { useQuery } from "@apollo/client";
+import { Row, Col, Container, Card} from "react-bootstrap";
+import {GET_ALL_USER_POSTS} from '../graphql/queries';
+
+
+function MyFeed() {
+
+    const { loading, error, data } = useQuery(GET_ALL_USER_POSTS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error loading posts :/ {error.message}</p>;
+
+    const posts = data.getallUserPosts
+
+  return (
+    <Container>
+      <h1 className= 'my-4'>My Feed</h1>
+
+        <Row>
+            {posts.map((post) => (
+            <Col md={4} sm="12" key={post._id} className="mb-4">
+
+                <Card className="h-100">
+                <Card.Body>
+                    <Card.Title>{post.title}</Card.Title>
+                    <Card.Text>{post.content}</Card.Text>
+                </Card.Body>
+                </Card>
+            </Col>
+            ))}
+        </Row>
+    </Container>
+  );
+}
+
+export default MyFeed;
